cf1473bc355e1f3279bcf7af837b94bb56151715
WyLax <artemarabik@gmail.com>
2025-10-28 20:06:35 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const hamburger = document.getElementById('hamburger');
+const name = document.getElementById('name');
 const menu = document.getElementById('menu');
+const menu2 = document.getElementById('menu2');
 const stick = document.getElementById('stick');
 
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('open');
+  menu2.classList.toggle('open');
   stick.classList.toggle('open');
-  hamburger.classList.toggle('active');
+  hamburger.classList.toggle('active')
+  name.classList.toggle('active');
 });
